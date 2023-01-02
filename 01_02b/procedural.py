ead2eafdb1ee5f8383038ce82991e67b98334b56
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2023-01-02 14:13:08 +0800

    Add files via upload

---
+++ 01_02b/procedural.py
@@ -0,0 +1,15 @@
+# Demonstrates procedural programming using Turtle graphics
+
+from turtle import *
+
+color("red")
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+done()
---
+++ 01_03b/basic_animation_example.py
@@ -0,0 +1,31 @@
+# Import the Turtle Graphics module
+import turtle
+
+# Define program constants
+WIDTH = 500
+HEIGHT = 500
+DELAY = 20 # Milliseconds between screen updates
+
+def move_turtle():
+	my_turtle.forward(1)
+	my_turtle.right(1)
+	screen.update()
+	screen.ontimer(move_turtle, DELAY)
+
+# Create a window where we will do our drawing.
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)  # Set the dimensions of the Turtle Graphics window.
+screen.title("Program Title")
+screen.bgcolor("cyan")
+screen.tracer(0) # Turns off automatic animation
+
+# Create a turtle to do your bidding
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("red")
+
+# Set animation in motion
+move_turtle()
+
+# This statement (or an equivalent) is needed at the end of all your turtle programs.
+turtle.done()
